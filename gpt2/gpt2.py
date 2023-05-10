703603dd7c58e7de01735a8ef9e395066e6e343f
honeyandme <2414342539@qq.com>
2023-05-10 16:37:04 +0800

    gpt2 load data

---
+++ gpt2/gpt2.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+import random
+from torch.utils.data import Dataset
+from datasets import load_dataset
+from transformers import BertTokenizerFast
+
+def tokenize(element):
+    outputs = tokenizer(element['content'],truncation=True,max_length=max_input_len,return_overflowing_tokens=True,return_length=True)
+    batch_input = []
+    for input_id,input_len in zip(outputs['input_ids'],outputs['length']):
+        if input_len == max_input_len:
+            batch_input.append(input_id)
+
+    return {"input_ids":batch_input}
+if __name__ == "__main__":
+    random.seed(3407)
+    file_path = glob(pathname=os.path.join('gpt2_data_mini','*','*'))
+
+    test_rate = 0.15
+    max_input_len = 128
+    test_data = random.sample(file_path,int(len(file_path)*test_rate))
+    train_data = [i for i in file_path if i not in test_data]
+
+    raw_dataset = load_dataset("csv",data_files={"train":train_data,"test":test_data},cache_dir="cache_dir")
+
+    tokenizer = BertTokenizerFast.from_pretrained(os.path.join('..','..','nlp','data','bert_base_chinese'))
+    tokenize_dataset = raw_dataset.map(tokenize,batched=True,remove_columns=raw_dataset['train'].column_names)
+
+    print()
