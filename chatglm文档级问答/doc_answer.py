fa225a39617c6f68f2e009c08e7321e928849fd7
honeyandme <2414342539@qq.com>
2023-05-31 18:38:11 +0800

    robot prompt

--- chatglm文档级问答/doc_answer.py
+++ chatglm文档级问答/doc_answer.py
@@ -3,6 +3,7 @@ from pypdf import PdfReader
 import docx
 import faiss
 from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer, AutoModel
 def get_data(path):
     all_content = []
     files = os.listdir(path)
@@ -44,12 +45,34 @@ class DFaiss:
     def search(self,text):
         text_emb = self.get_text_emb([text])
         D,I = self.index.search(text_emb,3)
-        return self.text_str_list[I[0][0]]
+        if D[0][0]<15:
+            return self.text_str_list[I[0][0]]
+        else:
+            return ""
+
+class prompt_robot():
+    def __init__(self):
+        self.myfaiss = DFaiss()
+        self.tokenizer = AutoTokenizer.from_pretrained("../THUDM/chatglm-6b", trust_remote_code=True)
+        self.model = AutoModel.from_pretrained("../THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+        self.model.eval()
+    def ask(self,query):
+        search_result = self.myfaiss.search(query)
+        if len(search_result)==0:
+            prompt = query
+        else:
+            prompt=f'请根据以下内容回答问题。内容是"{search_result}",问题是"{query}"。'
+        # print(f'the prompt is {prompt}')
+        response, history = self.model.chat(self.tokenizer, prompt, history=[])
+        return response
+    def add_content(self,strlist):
+        self.myfaiss.add_content(strlist)
+
 if __name__ == "__main__":
     all_content = get_data('datas')
-    myfaiss = DFaiss()
-    myfaiss.add_content(all_content)
+    robot = prompt_robot()
+    robot.add_content(all_content)
     while True:
+        print('----------------------------------')
         text = input('user:')
-
-        print(myfaiss.search(text))
+        print(f'robot:{robot.ask(text)}')
